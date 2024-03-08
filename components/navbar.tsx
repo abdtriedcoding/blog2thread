44dbc2d2a9125f2f53cbac7d8860f768eb404ccb
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-03-09 01:28:49 +0530

    add kinde auth

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <div className="z-50 bg-background fixed top-0 flex items-center w-full p-4 justify-between border-b">
-      <p className="font-semibold">Jotion</p>
-      <Button>Login</Button>
+      <Link href={"/"} className="font-semibold text-xl">
+        Blog2Thread
+      </Link>
+      {user ? (
+        <Button asChild>
+          <LogoutLink>Logout</LogoutLink>
+        </Button>
+      ) : (
+        <Button asChild>
+          <LoginLink>Log in</LoginLink>
+        </Button>
+      )}
     </div>
   );
 };
